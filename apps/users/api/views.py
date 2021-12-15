34b62ff2f00feaa45d1a49b21007f6d8b2764dfe
Mahdi Bahari <baharimahdi93@gmail.com>
2021-12-15 21:36:43 +0330

    celery config

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,5 +1,3 @@
-import hashlib
-import time
 from .userservice import JWTSimpleUserService
 from django.utils.translation import gettext as _
 from .serializers import *
@@ -8,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.throttling import UserRateThrottle
 
 
 class LoginRigisterRateThrottle(UserRateThrottle):
