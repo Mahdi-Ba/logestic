878210430b780bb92a093f91adb4d6ddf899272b
Mahdi Bahari <baharimahdi93@gmail.com>
2021-12-15 21:09:10 +0330

    apps

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,33 +1,47 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 
 class UserManager(BaseUserManager):
-    """Define a model manager for User"""
-
-    use_in_migrations = True
-
-    def _create_user(self, username, password, extra_fields):
-        """Create and save a User with the given username and password."""
-        user = self.model(username=username, **extra_fields)
+    def _create_user(self, username, password=None, extra_fields=None):
+        """
+        Creates and saves a User with the given username, date of
+        birth and password.
+        """
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, password, extra_fields):
-        extra_fields.pop('username')
-        extra_fields.pop('password')
-        """Create and save a regular User with the given username and password."""
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+    def create_user(self, username, password, extra_fields=None):
+        if extra_fields is not None:
+            extra_fields = dict(extra_fields)
+            if extra_fields != {}:
+                extra_fields.pop('username', None)
+                extra_fields.pop('password', None)
+                extra_fields.setdefault('is_staff', False)
+                extra_fields.setdefault('is_superuser', False)
+
         return self._create_user(username, password, extra_fields)
 
-    def create_superuser(self, username, password, **extra_fields):
-        """Create and save a SuperUser with the given mobile and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self._create_user(username, password, **extra_fields)
+    def create_superuser(self, username, password, email=None):
+        """
+        Creates and saves a superuser with the given username and password.
+        """
+        user = self._create_user(
+            username=username,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
-    """User model."""
     objects = UserManager()
+
+    def __str__(self):
+        return self.username
